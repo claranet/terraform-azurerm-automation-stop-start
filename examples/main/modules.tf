0e31e6564dcbc87073dec91d324c2dbadd6b76e7
Jérôme Respaut <jerome.respaut@claranet.com>
2024-10-21 16:03:33 +0200

    docs(AZ-111): rework README and example

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -1,41 +1,20 @@
 locals {
-  mysql_id = "/subscriptions/6b750f26-85f7-4089-886e-c8453a8cd96c/resourceGroups/rg-app-dha-dev/providers/Microsoft.DBforMySQL/flexibleServers/mydha"
-  vm_id    = "/subscriptions/6b750f26-85f7-4089-886e-c8453a8cd96c/resourceGroups/dha-test/providers/Microsoft.Compute/virtualMachines/dhavm"
-  aks_id   = "/subscriptions/6b750f26-85f7-4089-886e-c8453a8cd96c/resourcegroups/rg-kub-dha-dev/providers/Microsoft.ContainerService/managedClusters/aks-kub-dha-euw-dev"
+  mysql_id = "/subscriptions/12345678-abcd-1111-2222-000000000000/resourceGroups/rg-app-dha-dev/providers/Microsoft.DBforMySQL/flexibleServers/mydha"
+  vm_id    = "/subscriptions/12345678-abcd-1111-2222-000000000000/resourceGroups/dha-test/providers/Microsoft.Compute/virtualMachines/dhavm"
+  aks_id   = "/subscriptions/12345678-abcd-1111-2222-000000000000/resourcegroups/rg-kub-dha-dev/providers/Microsoft.ContainerService/managedClusters/aks-kub-dha-euw-dev"
 }
 
-module "azure_region" {
-  source  = "claranet/regions/azurerm"
+module "stop_start" {
+  source  = "claranet/automation-stop-start/azurerm"
   version = "x.x.x"
 
-  azure_region = var.azure_region
-}
-
-module "rg" {
-  source  = "claranet/rg/azurerm"
-  version = "x.x.x"
-
-  location    = module.azure_region.location
-  client_name = var.client_name
-  environment = var.environment
-  stack       = var.stack
-}
-
-data "azurerm_client_config" "current" {
-}
-
-module "resource_start_stop" {
-  source = "git@git.fr.clara.net:claranet/david.hautbois/start-stop-vm.git"
-
-
-  azure_subscription_id = data.azurerm_client_config.current.subscription_id
-  resource_group_name   = module.rg.resource_group_name
-  client_name           = var.client_name
-  location              = module.azure_region.location
-  location_short        = module.azure_region.location_short
-  location_cli          = module.azure_region.location_cli
-  environment           = var.environment
-  stack                 = var.stack
+  resource_group_name = module.rg.resource_group_name
+  client_name         = var.client_name
+  location            = module.azure_region.location
+  location_short      = module.azure_region.location_short
+  location_cli        = module.azure_region.location_cli
+  environment         = var.environment
+  stack               = var.stack
 
   schedules = {
     start = {
@@ -78,26 +57,33 @@ module "resource_start_stop" {
         local.vm_id,
         local.aks_id
       ],
-
     }
   }
 
+  logs_destinations_ids = [
+    module.logs.logs_storage_account_id,
+    module.logs.log_analytics_workspace_id
+  ]
+
+  extra_tags = {
+    foo = "bar"
+  }
 }
 
 resource "azurerm_role_assignment" "automation_vm" {
   scope                = local.vm_id
   role_definition_name = "Virtual Machine Contributor"
-  principal_id         = module.resource_start_stop.automation_identity
+  principal_id         = module.stop_start.identity_principal_id
 }
 
 resource "azurerm_role_assignment" "automation_mysql" {
   scope                = local.mysql_id
   role_definition_name = "Contributor"
-  principal_id         = module.resource_start_stop.automation_identity
+  principal_id         = module.stop_start.identity_principal_id
 }
 
 resource "azurerm_role_assignment" "automation_aks" {
   scope                = local.aks_id
   role_definition_name = "Contributor"
-  principal_id         = module.resource_start_stop.automation_identity
+  principal_id         = module.stop_start.identity_principal_id
 }
