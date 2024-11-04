91d5dcb2a3d0825209de3687dfca1db78f50d9ee
Jérôme Respaut <jerome.respaut@claranet.com>
2024-11-04 18:06:43 +0100

    refactor(AZ-111): lint json & ps1 files

--- files/start-stop-resource.ps1
+++ files/start-stop-resource.ps1
@@ -1,8 +1,8 @@
 Param(
-    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()]
     [String]
     $target_resource_id,
-    [Parameter(Mandatory=$true)][ValidateSet("start","stop")]
+    [Parameter(Mandatory = $true)][ValidateSet("start", "stop")]
     [String]
     $action
 )
@@ -12,12 +12,12 @@ Disable-AzContextAutosave -Scope Process | Out-Null
 
 # Connect using a Managed Service Identity
 try {
-        $AzureContext = (Connect-AzAccount -Identity).context
-    }
-catch{
-        Write-Output "There is no system-assigned user identity. Aborting.";
-        exit
-    }
+    $AzureContext = (Connect-AzAccount -Identity).context
+}
+catch {
+    Write-Output "There is no system-assigned user identity. Aborting.";
+    exit
+}
 
 Write-Output "Using system-assigned managed identity"
 
@@ -25,45 +25,45 @@ Write-Output "Using system-assigned managed identity"
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
 $resource_type = $(Get-AzResource -ResourceId $target_resource_id).ResourceType
+$resource_name = $(Get-AzResource -ResourceId $target_resource_id).Name
 
-Write-Output "Resource type : $resource_type"
+Write-Output "Resource type: $resource_type - Resource name: $resource_name"
 
-Switch ($resource_type)
-{
-    "Microsoft.Compute/virtualMachines"
-    {
-        Switch ($action)
-        {
-            "stop" { Stop-AzVM -Force -Id $target_resource_id }
-            "start" { Start-AzVM -Id $target_resource_id }
+Switch ($resource_type) {
+    "Microsoft.Compute/virtualMachines" {
+        Switch ($action) {
+            "stop" {
+                Write-Output "Stopping VM $resource_name"
+                Stop-AzVM -Force -Id $target_resource_id
+            }
+            "start" {
+                Write-Output "Starting VM $resource_name"
+                Start-AzVM -Id $target_resource_id
+            }
         }
     }
 
-    "Microsoft.DBforMySQL/flexibleServers"
-    {
-        Switch ($action)
-        {
+    "Microsoft.DBforMySQL/flexibleServers" {
+        Switch ($action) {
             "stop" {
-                Write-Output "Stopping mysql"
+                Write-Output "Stopping MySQL Flexible $resource_name"
                 Stop-AzMySqlFlexibleServer -InputObject $target_resource_id/stop
             }
             "start" {
-                Write-Output "Starting mysql"
-                Start-AzMySqlFlexibleServer -InputObject $target_resource_id/Start
+                Write-Output "Starting MySQL Flexible $resource_name"
+                Start-AzMySqlFlexibleServer -InputObject $target_resource_id/start
             }
         }
     }
 
-    "Microsoft.ContainerService/managedClusters"
-    {
-        Switch ($action)
-        {
+    "Microsoft.ContainerService/managedClusters" {
+        Switch ($action) {
             "stop" {
-                Write-Output "Stopping Kubernetes cluster"
+                Write-Output "Stopping AKS cluster $resource_name"
                 Get-AzAksCluster -Id $target_resource_id | Stop-AzAksCluster
             }
             "start" {
-                Write-Output "Starting Kubernetes cluster"
+                Write-Output "Starting AKS cluster $resource_name"
                 Get-AzAksCluster -Id $target_resource_id | Start-AzAksCluster
             }
         }
