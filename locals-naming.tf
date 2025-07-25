470c62f7a1bef66a0a106e047d3378678d128010
Maxime Poullain <maxime.poullain@fr.clara.net>
2025-07-25 10:58:54 +0200

    fix: use existing automation account name

--- locals-naming.tf
+++ locals-naming.tf
@@ -3,7 +3,7 @@ locals {
   name_prefix = lower(var.name_prefix)
   name_suffix = lower(var.name_suffix)
 
-  name                = coalesce(var.custom_name, data.azurecaf_name.automation.result)
+  name                = var.automation_account != null ? reverse(split("/", var.automation_account.id))[0] : coalesce(var.custom_name, data.azurecaf_name.automation.result)
   runbook_name        = coalesce(var.runbook_custom_name, data.azurecaf_name.runbook.result)
   api_connection_name = coalesce(var.api_connection_custom_name, data.azurecaf_name.api_connection.result)
   workflow_name = {
